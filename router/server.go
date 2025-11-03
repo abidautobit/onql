2fce36d1095f60263ea5b32fc0876d8556f23bb9
Utkarsh Singh <utkarsh190601@gmail.com>
2025-11-03 08:29:38 +0530

    Cleanup Router's Server

--- router/server.go
+++ router/server.go
@@ -1,15 +1,10 @@
 package router
 
-import (
-	"strings"
-)
+import "strings"
 
 var connQueryMap = make(map[string]*func(string))
 
-// var connMap = make(map[string]string)
-
 func HandleServerRequest(query string, connUniqueId string, responder *func(string)) {
-	//split query and create Message object
 	parts := strings.Split(query, "\x1E")
 	msg := &Message{
 		ID:      "server",
@@ -17,25 +12,15 @@ func HandleServerRequest(query string, connUniqueId string, responder *func(stri
 		Target:  parts[1],
 		Payload: parts[2],
 		Type:    "request",
-		// ConnId:  connUniqueId,
 	}
-	// store the responder function in the map
 	connQueryMap[msg.RID] = responder
-	// connMap[connUniqueId] = msg.RID
-	// connQueryMap[connUniqueId] = responder
-	// route the message
 	Route(msg)
 }
 
-// responsible for taking queries from tcp server and response them
+// Responsible for taking queries from tcp server and response them.
 func handleServerResponse(msg *Message) {
 	rid := msg.RID
-	//convert message to server response string
 	response := msg.RID + "\x1E" + msg.ID + "\x1E" + string(msg.Payload)
-	// print(response)
-	//send back to user and it will be handled by responder function
 	(*connQueryMap[rid])(response)
-	// if msg.ID != "subscribe" {
-	delete(connQueryMap, rid) // Remove the entry after responding
-	// }
+	delete(connQueryMap, rid)
 }
